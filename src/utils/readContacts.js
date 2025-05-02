442f0eb04daec0369b2033dff49bf6ac0165b5f7
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-05-02 13:14:04 +0300

    Update readContacts.js

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -2,5 +2,10 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const readContacts = async () => {
-  return fs.readFile(PATH_DB, { encoding: 'utf-8' });
+  try {
+    const contacts = fs.readFile(PATH_DB, { encoding: 'utf-8' });
+    console.log(contacts);
+  } catch (error) {
+    console.log(error);
+  }
 };
