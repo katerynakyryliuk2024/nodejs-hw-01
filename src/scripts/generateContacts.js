47bd772623938659e684d258f791994f793f8180
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-05-03 19:44:56 +0300

    created generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,15 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const randomGenerateContacts = [];
+  for (let i = 0; i <= number; i++) {
+    const contacts = createFakeContact();
+    randomGenerateContacts.push(contacts);
+  }
+  const newArr = readContacts.concat(randomGenerateContacts);
+  writeContacts(newArr);
+};
 
 generateContacts(5);
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -2,7 +2,7 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const readContacts = async () => {
-  const contacts = await fs.readFile(PATH_DB, 'utf-8');
-  const parseContacts = JSON.parse(contacts);
-  return console.log(parseContacts);
+  const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+
+  return console.log(contacts);
 };
