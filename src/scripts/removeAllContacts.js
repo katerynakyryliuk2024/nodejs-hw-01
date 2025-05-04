01a2d24ced1eb56b82d559428e74e2622719adf2
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-05-04 17:30:13 +0300

    update removeAllContacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -2,9 +2,10 @@ import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 export const removeAllContacts = async () => {
-  const contacts = await readContacts();
-  const newContacts = (contacts.lenght = 0);
-  return await writeContacts(newContacts);
+  let contacts = await readContacts();
+  contacts = [];
+  console.log(contacts);
+  return await writeContacts(contacts);
 };
 
 removeAllContacts();
